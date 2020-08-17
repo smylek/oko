9e3434038030e36447fd3cf66078d58ba0dbdb70
Eliasz <eliaszsliz@hotmail.com>
2020-08-17 13:46:59 +0200

    fix translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-xhr-backend";
+//import Backend from "i18next-xhr-backend";
 import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from 'assets/locales/EN/translation.json';
+import translationPL from 'assets/locales/PL/translation.json';
+
+// the translations
+const resources = {
+  EN: {
+    translation: translationEN
+  },
+  PL: {
+    translation: translationPL
+  }
+};
 
 i18n
   .use(LanguageDetector)
-  .use(Backend)
+  //.use(Backend)
   .use(initReactI18next)
   .init({
+    resources,
     lng: "EN",
     fallbackLng: 'EN',
     interpolation: {
